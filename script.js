eeec484aafc37a55a5a4e96a3df158ba666e5450
Louis Xia <23242389+xialouis1@users.noreply.github.com>
2024-03-19 17:53:26 -0400

    Update script.js
    Added comments to script.js to better outline flow of logic.

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ const suggestions = document.querySelector('.suggestions ul');
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search(str) {
+	// If parameter is empty, return an empty array.
 	if(str.length === 0) {
 		return [];
 	}
 
+	// Filter through fruit array to find parameter as a substring of a given element.
+	// return filtered array
 	return fruit.filter((fruit) => {
 		return fruit.toLowerCase().indexOf(str.toLowerCase()) !== -1;	
 	});
@@ -21,8 +24,11 @@ function searchHandler(e) {
 }
 
 function showSuggestions(results, inputVal) {
+	// Clear suggestions from DOM to avoid duplicate suggestions
 	suggestions.innerHTML = "";
 
+	// For each suggested result create an li element.
+	// Replace substring of suggested fruit with bold text
 	results.forEach((fruitName) => {
 		const suggestion = document.createElement("li");
 		const highlighted = fruitName.replace(
@@ -35,8 +41,11 @@ function showSuggestions(results, inputVal) {
 }
 
 function useSuggestion(e) {
+	// onClick, replace the input text with the target element's text.
 	const selectedText = e.target.innerText;
 	input.value = selectedText;
+
+	// Clear suggestions from DOM
 	showSuggestions([], selectedText);
 }
 
